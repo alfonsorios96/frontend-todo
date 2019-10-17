16831332df25689e6bd0708b2aca94d29451292a
Alfonso Ríos <manuelalfonso.rios.medellin.next@bbva.com>
2019-10-17 17:39:53 -0500

    task(*): Automatication developing task

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const resolve = require('rollup-plugin-node-resolve');
+const gulp = require('gulp');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const rollup = require('rollup');
+
+async function build() {
+  // see below for details on the options
+  const inputOptions = {
+    input: 'src/todo-app.js',
+    plugins: [resolve({
+      // pass custom options to the resolve plugin
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    })]
+  };
+  const outputOptions = {
+    file: 'dist/bundle.js',
+    format: 'cjs'
+  };
+
+  // create a bundle
+  const bundle = await rollup.rollup(inputOptions);
+
+  // generate code
+  const {output} = await bundle.generate(outputOptions);
+
+  // or write the bundle to disk
+  await bundle.write(outputOptions);
+}
+
+gulp.task('clean', () => {
+  return del('dist');
+});
+
+/**
+ * Bundle JS files starting from main.js
+ */
+gulp.task('rollup', ['clean'], async () => {
+  return await build();
+});
+
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('js-watch', ['rollup'], done => {
+  browserSync.reload();
+  done();
+});
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('default', ['rollup'], () => {
+
+  // Serve files from the root of this project
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  // add browserSync.reload to the tasks array to make
+  // all browsers reload after tasks are complete.
+  gulp.watch('src/**/*.js', ['js-watch']);
+});
--- src/todo-app.js
+++ src/todo-app.js
@@ -1,3 +1,6 @@
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-button/paper-button.js';
+
 class TodoApp extends HTMLElement {
 
   // WebComponents lifecycle
@@ -26,15 +29,30 @@ class TodoApp extends HTMLElement {
     const template = document.createElement('template');
 
     template.innerHTML = `
+        <style>
+            .success {
+                background-color: forestgreen;
+                color: white;
+            }
+            .error {
+                background-color: red;
+                color: white;
+            }
+        </style>
+        
         <h2>Tasks TO DO</h2>
+        <paper-input label="Task"></paper-input>
+        <paper-button class="success">Save</paper-button>
         <ul>
-            ${this.tasks.map(task => `<li>${task}</li>`)}
+            ${this.tasks.map(task => `
+              <li>${task} <paper-button class="error">Delete</paper-button></li>
+            `)}
         </ul>
       `;
 
     this.shadowRoot.innerHTML = '';
     this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.shadowRoot.innerHTML = this.shadowRoot.innerHTML.replaceAll('>,', '>');
+    this.shadowRoot.innerHTML = this.shadowRoot.innerHTML.replaceAll(',', '');
   }
 
   // Encapsulation for properties
